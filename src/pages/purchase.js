0bea47bec9309d93fadebea67b93336d3c8a5c7b
vihangatheturtle <vihanga1432@gmail.com>
2021-04-22 18:40:12 +0100

    Update purchase.js

--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -112,7 +112,7 @@ export default function Purchase({ release }) {
 }
 
 export async function getServerSideProps({ query }) {
-  const release = await retrieveRelease("ptest");
+  const release = await retrieveRelease("testp");
 
   if (!release) return {
     redirect: {
