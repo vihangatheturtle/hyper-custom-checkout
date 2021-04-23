2a97c8fe1bae64e80608f3fbcf1eb9b50ee9f6cf
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2021-04-23 20:02:18 +0100

    added frameParentComs.js to html

--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -56,6 +56,7 @@ export default function Purchase({ release }) {
 
   return (
   <body className="bg-transparent">
+    <script type="text/javascript" src="frameParentComs.js"></script>
     <div className="min-vh-100 d-flex align-items-center p-3">
       <div className="card rounded-lg mx-auto border" style={{ maxWidth: '28rem' }}>
         <div className="card-header bg-white py-3">
