4edc7ce1caa796be50a7dcd5b122abf4a5946643
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2021-04-23 19:52:28 +0100

    Update purchase.js

--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -60,9 +60,7 @@ export default function Purchase({ release }) {
       <div className="card rounded-lg mx-auto border" style={{ maxWidth: '28rem' }}>
         <div className="card-header bg-white py-3">
           <h4 className="mb-0">Purchase</h4>
-          <button type="button" className="close float-right" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+          <button type="button" className="btn-close float-right" aria-label="Close"></button>
         </div>
         <div className="card-body">
 
